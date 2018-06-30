ba7b654360e3cb4fd036c9d30430b8da5e826bb0
Tamino Martinius <dev@zaku.eu>
2018-06-30 18:06:37 +0200

    Mock https

--- src/__tests__/github.spec.ts
+++ src/__tests__/github.spec.ts
@@ -6,3 +6,50 @@ import {
 import https from 'https';
 import { dedent } from 'ts-dedent';
 
+jest.mock('https');
+let responseStatus: number = 902;
+let responseBody: Dict<any> = {};
+let requestString: string = '';
+let curlString: string = '';
+
+const requestMock = function (options, cb) {
+  let url = `${options.protocol}//`;
+  if (options.auth) url += `${options.auth}@`;
+  url += options.hostname;
+  if (options.port) url += `:${options.port}`;
+  url += options.path;
+  curlString +=
+    `curl --request ${options.method} --url '${url}' --header 'Content-Type: application/json'` +
+    ` --header 'Accept: application/json'`
+  ;
+  requestString = `${options.method} ${url}`;
+  const callbacks: Dict<any> = {};
+  cb({
+    on(event, cb) {
+      callbacks[event] = cb;
+    },
+    statusCode: responseStatus,
+  });
+  this.write = (str) => {
+    requestString += `\n${str}`;
+    curlString += ` --data '${str}'`;
+  };
+  this.end = () => {
+    if (callbacks.data) {
+      callbacks.data(JSON.stringify(responseBody));
+      callbacks.end();
+    }
+  };
+  return this;
+};
+
+/// @ts-ignore
+https.request.mockImplementation(requestMock);
+
+beforeEach(() => {
+  responseStatus = 902;
+  responseBody = {};
+  requestString = '';
+  curlString = '';
+});
+
