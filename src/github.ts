a63f37070436790dd6c0659b4afe95f9eee1c639
Tamino Martinius <dev@zaku.eu>
2018-06-30 18:05:31 +0200

    Add constructor

--- src/github.ts
+++ src/github.ts
@@ -6,6 +6,18 @@ import { request } from 'https';
 import { URL } from 'url';
 
 export class GitHub {
+  token: string;
+  debug: boolean = false;
+  apiUrl: string = 'https://api.github.com/graphql';
+  url: URL;
+
+  constructor(config: Config) {
+    this.token = config.token;
+    if (config.debug) this.debug = config.debug;
+    if (config.apiUrl) this.apiUrl = config.apiUrl;
+    this.url = new URL(this.apiUrl);
+  }
+
 }
 
 export default GitHub;
