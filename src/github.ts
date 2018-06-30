54d8e92cae4b23a6e20fb7a124e7825aa4575685
Tamino Martinius <dev@zaku.eu>
2018-06-30 18:06:52 +0200

    Implement query

--- src/github.ts
+++ src/github.ts
@@ -18,6 +18,65 @@ export class GitHub {
     this.url = new URL(this.apiUrl);
   }
 
+  public async query(query: string, variables?: Dict<any>): Promise<any> {
+    const payload = {
+      query,
+      variables,
+    };
+
+    const payloadString = JSON.stringify(payload);
+
+    return new Promise((resolve, reject) => {
+      const req = request(
+        {
+          hostname: 'api.github.com',
+          path: '/graphql',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': payloadString.length,
+            Authorization: 'bearer ' + this.token,
+            'User-Agent': 'GitHub GraphQL Client',
+          },
+        },
+        (res) => {
+          const chunks: string[] = [];
+
+          res.on('data', (chunk) => {
+            chunks.push((<Buffer>chunk).toString('utf8'));
+          });
+
+          res.on('end', () => {
+            if (res.statusCode !== 200) {
+              return reject(res.statusMessage);
+            }
+
+            const response = chunks.join('');
+            let json: any;
+
+            try {
+              json = JSON.parse(response);
+            } catch (e) {
+              return reject('GitHub GraphQL API response is not able to be parsed as JSON');
+            }
+
+            if (!json.data) {
+              if (json.errors) {
+                return reject(json.errors);
+              }
+              return reject('Unknown GraphQL error');
+            }
+
+            return resolve(json);
+          });
+        },
+      );
+
+      req.on('error', (err) => { reject(err); });
+      req.write(payloadString);
+      req.end();
+    });
+  }
 }
 
 export default GitHub;
